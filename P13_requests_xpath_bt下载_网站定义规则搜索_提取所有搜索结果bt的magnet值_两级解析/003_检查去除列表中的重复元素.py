8bd59e3a842d8d5b015563e1cf8be0be6eb6f871
Felix Zhang <18200116656@qq.com>
2019-09-18 22:34:59 +0800

    Daily Commit

--- P13_requests_xpath_bt下载_网站定义规则搜索_提取所有搜索结果bt的magnet值_两级解析/003_检查去除列表中的重复元素.py
+++ P13_requests_xpath_bt下载_网站定义规则搜索_提取所有搜索结果bt的magnet值_两级解析/003_检查去除列表中的重复元素.py
@@ -1,8 +1,14 @@
 
 
-with open('tushy.txt', 'r', encoding='utf-8') as f:
+with open('new.txt', 'r', encoding='utf-8') as f:
     content = f.readlines()
     print(content)
     print(len(content))
     list_new = sorted(set(content), key=content.index)
     print(len(list_new))
+
+with open('new_tushy.txt', 'w', encoding='utf-8') as f:
+    for i in list_new:
+        f.write(i)
+
+
